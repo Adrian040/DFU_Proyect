c2b6cf08b7fb325b8ae89fe427f42c9b3dba0f5e
Adrian Maldonado <am96929@gmal.com>
2024-12-28 04:03:38 -0600

    feat: Agrega el script tets.py para cargar el modelo guardado y hacer el test con el test set.

---
+++ SegmentationNetworks/Models/Unet/test.py
@@ -0,0 +1,40 @@
+import json
+import torch
+import pandas as pd
+from main import UNET
+from metrics import calculate_metrics
+
+# ------------- Parámetros ----------------
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+batch_size = 4
+test_image_dir = "C:/Users/am969/Documents/DFU_Proyect/SegmentationNetworks/data_DFU_images/data_MICCAI/test_images"
+test_mask_dir = "C:/Users/am969/Documents/DFU_Proyect/SegmentationNetworks/data_DFU_images/data_MICCAI/test_masks"
+
+# ----- Cargamos el modelo entrenado con las mejores métricas ----------
+checkpoint = torch.load("output_assets_model/best_model_checkpoint.pth")
+model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+model.load_state_dict(checkpoint["state_dict"])
+model.eval()
+
+# ----- Calculamos las métricas --------------
+
+accuracy, dice_coefficient, precision, recall, f1_score = calculate_metrics(test_image_dir, test_mask_dir, model, device=DEVICE, image_height=240, image_width=240)
+
+# ----- Guardamos las métricas en un archivo .csv --------------
+test_metrics = {
+    "Accuracy": accuracy,
+    "Dice Coefficient": dice_coefficient,
+    "Precision": precision,
+    "Recall": recall,
+    "F1 Score": f1_score
+}
+pd.DataFrame(test_metrics, index=[0]).to_csv("output_assets_model/test_metrics.csv", index=False)
+# with open("output_assets_model/test_metrics.json", "w") as outfile: # Guardar las métricas en un archivo JSON
+#     json.dump(test_metrics, outfile)
+
+# ----- Imprimimos las métricas (opcional) --------------
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Dice Coefficient: {dice_coefficient:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1 Score: {f1_score:.4f}")
