f3329f310acbced7d1e47ea8307ee110a0cee0ac
Adrian Maldonado <am96929@gmal.com>
2025-01-04 04:18:27 -0600

    feat: Implementa el script para la etapa de test (DFUTissueSegNet)

---
+++ ClasificationAlgorithms/Models/Unet/test.py
@@ -0,0 +1,33 @@
+import json
+import torch
+import pandas as pd
+from main import UNET #, ResUnet, etc.
+from metrics import calculate_metrics
+
+# ------------- Parámetros ----------------
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+batch_size = 4
+img_size_for_test = 240
+test_image_dir = "C:/Users/am969/Documents/DFU_Proyect/SegmentationNetworks/data_DFU_images/data_MICCAI/test_images"
+test_mask_dir = "C:/Users/am969/Documents/DFU_Proyect/SegmentationNetworks/data_DFU_images/data_MICCAI/test_masks"
+
+# ----- Cargamos el modelo entrenado con las mejores métricas ----------
+checkpoint = torch.load("output_assets_model/best_model_checkpoint.pth", weights_only=True)  ## Nota: el argumento weights_only=True es para evitar el warning que indica que de esta forma se carga con mayor seguridad el modelo. Sin embargo no se están cargando otros datos como el optimizador. En resumen, esto es solo para quitar el warning pues en principio no hay datos maliciosos en la forma en que se guarda el modelo localmente.
+model = UNET(in_channels=3, out_channels=4).to(DEVICE)    ## ------------ Aquí al cambiar de modelo -------------.
+model.load_state_dict(checkpoint["state_dict"])
+model.eval()
+
+# ----- Calculamos las métricas --------------
+
+print("Calculating test metrics...")
+dict_test_metrics = calculate_metrics(test_image_dir, test_mask_dir, model, num_classes=4, device=DEVICE, image_height=img_size_for_test, image_width=img_size_for_test)
+
+# ----- Guardamos las métricas en un archivo .csv --------------
+pd.DataFrame(dict_test_metrics).to_csv("output_assets_model/test_metrics.csv", index=False) # Sin índices.
+# # Guardar las métricas en un archivo JSON
+# with open("output_assets_model/test_metrics.json", "w") as outfile:
+#     json.dump(test_metrics, outfile)
+
+# ----- Imprimimos las métricas (opcional) --------------
+print("Métricas calculadas para el test set:")
+print(pd.DataFrame(dict_test_metrics))
