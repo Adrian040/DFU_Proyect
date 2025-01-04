9b4c0dfc7471ed71a099e272627883670fe49178
Adrian Maldonado <am96929@gmal.com>
2025-01-04 03:09:32 -0600

    doc: Agrega docstrings a las funciones de metrics.py. Además verifica que esté bien implementada la dice_loss_multiclass function (función de pérdida).

--- ClasificationAlgorithms/Models/Unet/metrics.py
+++ ClasificationAlgorithms/Models/Unet/metrics.py
@@ -3,8 +3,8 @@ import torch.nn.functional as F
 import numpy as np
 from utils import get_test_loader
 
-def check_metrics(loader, model, device="cuda"):
-    num_classes = 4
+def check_metrics(loader, model, num_classes = 4, device="cuda"):
+    """Calcula las métricas de evaluación de un modelo de segmentación multiclase (opcional) en un conjunto de datos de validación."""
     metrics = {
         "dice_coefficient": torch.zeros(num_classes, device=device),
         "IoU": torch.zeros(num_classes, device=device),
@@ -55,40 +55,17 @@ def check_metrics(loader, model, device="cuda"):
     model.train()
     return dict_metrics
 
-def calculate_metrics(test_image_dir, test_mask_dir, model, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), image_height=240, image_width=240, num_workers=0, batch_size=4, pin_memory=True):
-    num_correct = 0
-    num_pixels = 0
-    dice_score = 0
-    true_positive = 0
-    false_positive = 0
-    false_negative = 0
+def calculate_metrics(test_image_dir, test_mask_dir, model, num_classes=4, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), image_height=240, image_width=240, num_workers=0, batch_size=4, pin_memory=True):
+    """Hace lo mismo que check metrics, pero en este se usa el test_loader para el cálculo de las métricas del test."""
     model.eval()
+    loader = get_test_loader(test_image_dir, test_mask_dir, batch_size= batch_size,  image_height=image_height, image_width=image_width, num_workers=num_workers, pin_memory=pin_memory)   # Cargar los datos.
 
-    loader = get_test_loader(test_image_dir, test_mask_dir, batch_size= batch_size,  image_height=image_height, image_width=image_width, num_workers=num_workers, pin_memory=pin_memory)
-
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device)
-            y = y.to(device).unsqueeze(1)
-            preds = torch.sigmoid(model(x))
-            preds = (preds > 0.5).float()
-            num_correct += (preds == y).sum()
-            num_pixels += torch.numel(preds)
-            dice_score += (2 * (preds * y).sum()) / ((preds + y).sum() + 1e-8)
-            true_positive += ((preds == 1) & (y == 1)).sum()
-            false_positive += ((preds == 1) & (y == 0)).sum()
-            false_negative += ((preds == 0) & (y == 1)).sum()
-
-    dice_coefficient = dice_score / len(loader) if len(loader) > 0 else 0
-    IoU = true_positive / (true_positive + false_positive + false_negative) if (true_positive + false_positive + false_negative) > 0 else 0
-    accuracy = num_correct / num_pixels if num_pixels > 0 else 0
-    precision = true_positive / (true_positive + false_positive) if (true_positive + false_positive) > 0 else 0
-    recall = true_positive / (true_positive + false_negative) if (true_positive + false_negative) > 0 else 0
-    f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
-
+    dict_metrics = check_metrics(loader, model, num_classes=num_classes, device=device)  # Calcular las métricas.
+    
     model.train() # regresarlo a su estado original si se quiere seguir entrenando el modelo.
 
-    return dice_coefficient.item(), IoU.item(), accuracy.item(), precision.item(), recall.item(), f1_score.item()
+    return dict_metrics
+    # return dice_coefficient.item(), IoU.item(), accuracy.item(), precision.item(), recall.item(), f1_score.item()
 
 def dice_loss(input, target):
     smooth = 1.0
@@ -143,18 +120,12 @@ def dice_loss_multiclass(pred, target, epsilon=1e-6):
     if target.dtype != torch.long:
         target = target.long()
 
-    # Verifica dimensiones del target antes de one-hot
-    # print(f"Dimensiones del target antes de one-hot: {target.shape}")
-
     # Aplicar Softmax a las predicciones para convertir logits a probabilidades
     pred = F.softmax(pred, dim=1)
 
     # Convertir las etiquetas a one-hot encoding y ajustar dimensiones
     target_one_hot = F.one_hot(target, num_classes=pred.shape[1])  # (batch_size, H, W, num_classes)
     target_one_hot = target_one_hot.squeeze(1).permute(0, 3, 1, 2).float()  # (batch_size, num_classes, H, W)
-
-    # Verifica dimensiones después del procesamiento
-    # print(f"Dimensiones del target después de one-hot: {target_one_hot.shape}")
 
     # Calcular el Dice para cada clase
     intersection = torch.sum(pred * target_one_hot, dim=(2, 3))
